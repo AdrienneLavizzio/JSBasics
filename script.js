bafff1952f75aff35a2ce134d3f838fcd9bbe289
“AdrienneLavizzio” <adriennelavizzio@gmail.com>
2022-03-03 00:30:16 -0500

    script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+//1
+console.log(13);
+//2
+/*
+strings
+boolean
+nulls
+numbers
+undefined
+*/
+
+//3
+/* 
+Addition +
+Subtraction -
+Division /
+Multiplication *
+Module %
+Exponentiation **
+*/
+
+//4
+console.log(5%4);
+console.log(5%2);
+
+//5
+console.log('I\'M MARRY POPPINS,Y\'ALL!');
+
+//6
+console.log("Hawk"+"eye");
+
+
+//7
+console.log("Adrienne".length + "Lavizzio".length);
+
+//8
+console.log("Avengers: Endgame"[16]);
